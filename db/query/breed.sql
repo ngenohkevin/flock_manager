7fc653cfc2d9e9d80474dd10877d5eef30e856a5
ngenohkevin <ngenohkevin19@gmail.com>
2023-03-10 14:51:16 +0300

    sqlc generated code

--- db/query/breed.sql
+++ db/query/breed.sql
@@ -5,3 +5,23 @@ INSERT INTO breed (
 ) VALUES (
            $1
 ) RETURNING *;
+
+-- name: GetBreed :one
+SELECT * FROM breed
+WHERE breed_id = $1 LIMIT 1;
+
+-- name: ListBreeds :many
+SELECT * FROM breed
+ORDER BY breed_id
+    LIMIT $1
+OFFSET $2;
+
+-- name: UpdateBreed  :one
+UPDATE breed
+set breed_name = $2
+WHERE breed_id = $1
+    RETURNING *;
+
+-- name: DeleteBreed :exec
+DELETE FROM breed
+WHERE breed_id = $1;
