aaf3645bdb6f958739a7c4866279ad5950a90510
ngenohkevin <ngenohkevin19@gmail.com>
2023-03-14 15:50:48 +0300

    created sqlc schema for premises

---
+++ db/query/premises.sql
@@ -0,0 +1,27 @@
+-- name: CreatePremises :one
+
+INSERT INTO premises (premises_id, farm, house)
+VALUES ($1, $2, $3)
+RETURNING *;
+
+-- name: GetPremises :one
+SELECT * FROM premises
+WHERE premises_id = $1 LIMIT 1;
+
+-- name: ListPremises :many
+SELECT * FROM premises
+WHERE premises_id = $1
+ORDER BY id
+LIMIT $2
+OFFSET $3;
+
+-- name: UpdatePremises :one
+UPDATE premises
+SET farm = $2,
+    house = $3
+WHERE id = $1
+RETURNING *;
+
+-- name: DeletePremises :exec
+DELETE FROM premises
+WHERE id = $1;
