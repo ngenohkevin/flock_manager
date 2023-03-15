a2a2b68f4e4e62fed7ec8ddaa5454fca05dff7ad
ngenohkevin <ngenohkevin19@gmail.com>
2023-03-15 13:52:34 +0300

    testcases for breed

--- db/sqlc/breed_test.go
+++ db/sqlc/breed_test.go
@@ -2,9 +2,11 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"github.com/ngenohkevin/flock_manager/db/util"
 	"github.com/stretchr/testify/require"
 	"testing"
+	"time"
 )
 
 func createdRandomBreed(t *testing.T) Breed {
@@ -24,4 +26,65 @@ func createdRandomBreed(t *testing.T) Breed {
 
 func TestCreateAccount(t *testing.T) {
 	createdRandomBreed(t)
+}
+
+func TestGetBreed(t *testing.T) {
+	breed1 := createdRandomBreed(t)
+	breed2, err := testQueries.GetBreed(context.Background(), breed1.BreedID)
+	require.NoError(t, err)
+
+	require.Equal(t, breed1.BreedID, breed2.BreedID)
+	require.Equal(t, breed1.BreedName, breed2.BreedName)
+
+	require.WithinDuration(t, breed1.CreatedAt, breed2.CreatedAt, time.Second)
+}
+
+func TestUpdateBreed(t *testing.T) {
+	breed1 := createdRandomBreed(t)
+
+	arg := UpdateBreedParams{
+		BreedID:   breed1.BreedID,
+		BreedName: util.RandomBreed(),
+	}
+
+	breed2, err := testQueries.UpdateBreed(context.Background(), arg)
+	require.NoError(t, err)
+
+	require.NotEmpty(t, breed2)
+
+	require.Equal(t, breed1.BreedID, breed2.BreedID)
+	require.Equal(t, arg.BreedID, breed2.BreedID)
+
+	require.WithinDuration(t, breed1.CreatedAt, breed2.CreatedAt, time.Second)
+
+}
+
+func TestListBreeds(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		createdRandomBreed(t)
+	}
+	arg := ListBreedsParams{
+		Limit:  5,
+		Offset: 5,
+	}
+
+	breeds, err := testQueries.ListBreeds(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, breeds, 5)
+
+	for _, breed := range breeds {
+		require.NotEmpty(t, breed)
+	}
+}
+
+func TestDeleteBreed(t *testing.T) {
+	breed1 := createdRandomBreed(t)
+	err := testQueries.DeleteBreed(context.Background(), breed1.BreedID)
+	require.NoError(t, err)
+
+	breed2, err := testQueries.GetBreed(context.Background(), breed1.BreedID)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+
+	require.Empty(t, breed2)
 }
