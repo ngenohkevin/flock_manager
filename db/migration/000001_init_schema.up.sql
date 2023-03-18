8a1e84db370164656aaa738b8e902a8b23d3955c
ngenohkevin <ngenohkevin19@gmail.com>
2023-03-18 16:34:52 +0300

    redesigned the database

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -1,12 +1,12 @@
 CREATE TABLE "breed" (
-                         "breed_id" bigserial PRIMARY KEY,
+                         "breed_id" BIGSERIAL PRIMARY KEY,
                          "breed_name" varchar UNIQUE NOT NULL,
                          "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE "production" (
-                              "id" bigserial PRIMARY KEY,
-                              "production_id" bigint UNIQUE NOT NULL,
+                              "id" BIGSERIAL PRIMARY KEY,
+                              "breed_id" bigint NOT NULL,
                               "eggs" bigint NOT NULL,
                               "dirty" bigint NOT NULL,
                               "wrong_shape" bigint NOT NULL,
@@ -17,8 +17,8 @@ CREATE TABLE "production" (
 );
 
 CREATE TABLE "hatchery" (
-                            "id" bigserial PRIMARY KEY,
-                            "hatchery_id" bigint NOT NULL,
+                            "id" BIGSERIAL PRIMARY KEY,
+                            "production_id" bigint NOT NULL,
                             "infertile" bigint NOT NULL,
                             "early" bigint NOT NULL,
                             "middle" bigint NOT NULL,
@@ -29,8 +29,8 @@ CREATE TABLE "hatchery" (
 );
 
 CREATE TABLE "premises" (
-                            "id" bigserial PRIMARY KEY,
-                            "premises_id" bigint NOT NULL,
+                            "id" BIGSERIAL PRIMARY KEY,
+                            "breed_id" bigint NOT NULL,
                             "farm" varchar NOT NULL,
                             "house" varchar NOT NULL,
                             "created_at" timestamptz NOT NULL DEFAULT (now())
@@ -38,14 +38,14 @@ CREATE TABLE "premises" (
 
 CREATE INDEX ON "breed" ("breed_name");
 
-CREATE INDEX ON "production" ("production_id");
+CREATE INDEX ON "production" ("breed_id");
 
-CREATE INDEX ON "hatchery" ("hatchery_id");
+CREATE INDEX ON "hatchery" ("production_id");
 
-CREATE INDEX ON "premises" ("premises_id");
+CREATE INDEX ON "premises" ("breed_id");
 
-ALTER TABLE "production" ADD FOREIGN KEY ("production_id") REFERENCES "breed" ("breed_id");
+ALTER TABLE "production" ADD FOREIGN KEY ("breed_id") REFERENCES "breed" ("breed_id");
 
-ALTER TABLE "hatchery" ADD FOREIGN KEY ("hatchery_id") REFERENCES "production" ("production_id");
+ALTER TABLE "hatchery" ADD FOREIGN KEY ("production_id") REFERENCES "production" ("id");
 
-ALTER TABLE "premises" ADD FOREIGN KEY ("premises_id") REFERENCES "breed" ("breed_id");
+ALTER TABLE "premises" ADD FOREIGN KEY ("breed_id") REFERENCES "breed" ("breed_id");
