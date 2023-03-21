94e6a85e1e6b89c7b4a9fb30cf9ab7932b51cc5e
ngenohkevin <ngenohkevin19@gmail.com>
2023-03-21 15:33:32 +0300

    connecting the database and the server

--- main.go
+++ main.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+	"github.com/ngenohkevin/flock_manager/api"
+	db "github.com/ngenohkevin/flock_manager/db/sqlc"
+	"log"
+)
+
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://root:85dilanwest@localhost:5432/flock_manager?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
+
+func main() {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("Error connecting to the database", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal("unable to start the server", err)
+	}
+}
