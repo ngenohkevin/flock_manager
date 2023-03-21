0a96b34fd8c55f338cb1f875495ff30a33223d2f
ngenohkevin <ngenohkevin19@gmail.com>
2023-03-21 15:29:19 +0300

    api routes for breed

--- api/breed.go
+++ api/breed.go
@@ -1,1 +1,75 @@
 package api
+
+import (
+	"database/sql"
+	"github.com/gin-gonic/gin"
+	db "github.com/ngenohkevin/flock_manager/db/sqlc"
+	"net/http"
+)
+
+type createBreedRequest struct {
+	Breed string `json:"breed" binding:"required"`
+}
+
+func (server *Server) createBreed(ctx *gin.Context) {
+	var req createBreedRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+	breed, err := server.store.CreateBreed(ctx, req.Breed)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, breed)
+}
+
+type getBreedRequest struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+func (server *Server) getBreed(ctx *gin.Context) {
+	var req getBreedRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+	breed, err := server.store.GetBreed(ctx, req.ID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, breed)
+}
+
+type listBreedRequest struct {
+	PageID   int32 `form:"page_id" binding:"required,min=1"`
+	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
+}
+
+func (server *Server) listBreeds(ctx *gin.Context) {
+	var req listBreedRequest
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+	arg := db.ListBreedsParams{
+		Limit:  req.PageSize,
+		Offset: (req.PageID - 1) * req.PageSize,
+	}
+
+	breeds, err := server.store.ListBreeds(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, breeds)
+}
+
+type updateBreedRequest struct {
+}
