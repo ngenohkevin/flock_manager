0be9f49ffb8a2a8a69654915aad7f6a3d78b7c31
ngenohkevin <ngenohkevin19@gmail.com>
2023-03-23 20:18:47 +0300

    update breeds handler

--- api/breed.go
+++ api/breed.go
@@ -76,7 +76,7 @@ type updateBreedRequest struct {
 	BreedName string `json:"breed_name" binding:"required"`
 }
 
-//update breeds handler
+// update breeds handler
 func (server *Server) updateBreed(ctx *gin.Context) {
 	var req updateBreedRequest
 
@@ -99,4 +99,30 @@ func (server *Server) updateBreed(ctx *gin.Context) {
 		return
 	}
 	ctx.JSON(http.StatusOK, breeds)
+}
+
+type deleteBreedsRequest struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+func (server *Server) deleteBreed(ctx *gin.Context) {
+	var req deleteBreedsRequest
+
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	err := server.store.DeleteBreed(ctx, req.ID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{
+		"success": "Deleted",
+	})
 }
