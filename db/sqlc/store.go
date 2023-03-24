6fe5e96c237a04f5d4d279bb6017a4823cab3de9
ngenohkevin <ngenohkevin19@gmail.com>
2023-03-24 17:55:32 +0300

    enabled mockgen in the testcases

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -2,13 +2,18 @@ package db
 
 import "database/sql"
 
-type Store struct {
+type Store interface {
+	Querier
+}
+
+// SQLStore provides all functions to execute SQL queries and transactions
+type SQLStore struct {
 	*Queries
 	db *sql.DB
 }
 
-func NewStore(db *sql.DB) *Store {
-	return &Store{
+func NewStore(db *sql.DB) Store {
+	return &SQLStore{
 		db:      db,
 		Queries: New(db),
 	}
