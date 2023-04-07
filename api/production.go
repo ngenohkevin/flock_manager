abd484991229db1b9ebebc0142220b8e2ae4090a
ngenohkevin <ngenohkevin19@gmail.com>
2023-04-07 16:30:54 +0300

    fixed list production

--- api/production.go
+++ api/production.go
@@ -69,8 +69,13 @@ func (server *Server) getProduction(ctx *gin.Context) {
 }
 
 type listProductionRequest struct {
+	ID       int64 `json:"id"`
 	PageID   int32 `form:"page_id" binding:"required,min=1"`
 	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
+}
+
+type listIDProduction struct {
+	ID int64 `json:"id"  binding:"required"`
 }
 
 //list production handler
@@ -78,11 +83,16 @@ type listProductionRequest struct {
 
 func (server *Server) listProduction(ctx *gin.Context) {
 	var req listProductionRequest
+	//var id listIDProduction
 
 	if err := ctx.ShouldBindQuery(&req); err != nil {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
+	//if err := ctx.ShouldBindUri(&id); err != nil {
+	//	ctx.JSON(http.StatusBadRequest, errorResponse(err))
+	//	return
+	//}
 	arg := db.ListProductionParams{
 		Limit:  req.PageSize,
 		Offset: (req.PageID - 1) * req.PageSize,
