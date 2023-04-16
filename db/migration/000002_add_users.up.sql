897f2d8c2feebd4e5c4bd1c7fc40748517092180
ngenohkevin <ngenohkevin19@gmail.com>
2023-04-16 22:33:20 +0300

    migration up for users

---
+++ db/migration/000002_add_users.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "users" (
+                         "username" varchar PRIMARY KEY,
+                         "hashed_password" varchar NOT NULL,
+                         "full_name" varchar NOT NULL,
+                         "email" varchar UNIQUE NOT NULL,
+                         "password_changed_at" timestamptz NOT NULL DEFAULT '0001-01-01 00:00:00Z',
+                         "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+ALTER TABLE "breed" ADD FOREIGN KEY ("breed_name") REFERENCES "users" ("username");
