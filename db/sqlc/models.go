d29d7a25da84b59ba33be54c34b82abf30dcfdf4
ngenohkevin <ngenohkevin19@gmail.com>
2023-04-17 13:36:59 +0300

    sqlc generated code

--- db/sqlc/models.go
+++ db/sqlc/models.go
@@ -45,3 +45,12 @@ type Production struct {
 	HatchingEggs int64     `json:"hatching_eggs"`
 	CreatedAt    time.Time `json:"created_at"`
 }
+
+type User struct {
+	Username          string    `json:"username"`
+	HashedPassword    string    `json:"hashed_password"`
+	FullName          string    `json:"full_name"`
+	Email             string    `json:"email"`
+	PasswordChangedAt time.Time `json:"password_changed_at"`
+	CreatedAt         time.Time `json:"created_at"`
+}
