8822786bdd054fceae06c8ae4e87a0e2dd8cb6c2
ngenohkevin <ngenohkevin19@gmail.com>
2023-04-20 23:23:22 +0300

    integrated middleware auth

--- api/user.go
+++ api/user.go
@@ -3,7 +3,6 @@ package api
 import (
 	"database/sql"
 	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
 	"github.com/lib/pq"
 	db "github.com/ngenohkevin/flock_manager/db/sqlc"
 	"github.com/ngenohkevin/flock_manager/db/util"
@@ -77,12 +76,12 @@ type loginUserRequest struct {
 	Password string `json:"password" binding:"required,min=6"`
 }
 type loginUserResponse struct {
-	SessionID             uuid.UUID    `json:"session_id"`
-	AccessToken           string       `json:"access_token"`
-	AccessTokenExpiresAt  time.Time    `json:"access_token_expires_at"`
-	RefreshToken          string       `json:"refresh_token"`
-	RefreshTokenExpiresAt time.Time    `json:"refresh_token_expires_at"`
-	User                  userResponse `json:"user"`
+	//SessionID             uuid.UUID    `json:"session_id"`
+	AccessToken string `json:"access_token"`
+	//AccessTokenExpiresAt  time.Time    `json:"access_token_expires_at"`
+	//RefreshToken          string       `json:"refresh_token"`
+	//RefreshTokenExpiresAt time.Time    `json:"refresh_token_expires_at"`
+	User userResponse `json:"user"`
 }
 
 func (server *Server) loginUser(ctx *gin.Context) {
