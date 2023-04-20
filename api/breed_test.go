970b6f577eb3946ab0525f4a7dc699b642293795
ngenohkevin <ngenohkevin19@gmail.com>
2023-04-20 23:25:20 +0300

    middleware integration and testcases

--- api/breed_test.go
+++ api/breed_test.go
@@ -5,15 +5,18 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"github.com/golang/mock/gomock"
 	mockdb "github.com/ngenohkevin/flock_manager/db/mock"
 	db "github.com/ngenohkevin/flock_manager/db/sqlc"
 	"github.com/ngenohkevin/flock_manager/db/util"
+	"github.com/ngenohkevin/flock_manager/token"
 	"github.com/stretchr/testify/require"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
 // GetBreed tests
@@ -22,14 +25,19 @@ func TestGetBreedAPI(t *testing.T) {
 	breed := randomBreed(user.Username)
 
 	testCases := []struct {
-		name          string
-		BreedID       int64
+		name      string
+		BreedID   int64
+		setupAuth func(t *testing.T, request *http.Request, tokenMaker token.Maker)
+
 		buildStubs    func(store *mockdb.MockStore)
 		checkResponse func(t *testing.T, recorder *httptest.ResponseRecorder)
 	}{
 		{
 			name:    "OK",
 			BreedID: breed.BreedID,
+			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+				addAuthorization(t, request, tokenMaker, authorizationTypeBearer, user.Username, time.Minute)
+			},
 			buildStubs: func(store *mockdb.MockStore) {
 				store.EXPECT().GetBreed(gomock.Any(), gomock.Eq(breed.BreedID)).Times(1).
 					Return(breed, nil)
@@ -40,8 +48,41 @@ func TestGetBreedAPI(t *testing.T) {
 			},
 		},
 		{
+			name:    "UnauthorizedUser",
+			BreedID: breed.BreedID,
+			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+				addAuthorization(t, request, tokenMaker, authorizationTypeBearer, "unauthorized_user", time.Minute)
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetBreed(gomock.Any(), gomock.Eq(breed.BreedID)).
+					Times(1).
+					Return(breed, nil)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusUnauthorized, recorder.Code)
+			},
+		},
+		{
+			name:    "NoAuthorization",
+			BreedID: breed.BreedID,
+			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetBreed(gomock.Any(), gomock.Any()).
+					Times(0)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusUnauthorized, recorder.Code)
+			},
+		},
+		{
 			name:    "NotFound",
 			BreedID: breed.BreedID,
+			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+				addAuthorization(t, request, tokenMaker, authorizationTypeBearer, "user", time.Minute)
+			},
 			buildStubs: func(store *mockdb.MockStore) {
 				store.EXPECT().GetBreed(gomock.Any(), gomock.Eq(breed.BreedID)).Times(1).
 					Return(db.Breed{}, sql.ErrNoRows)
@@ -53,6 +94,9 @@ func TestGetBreedAPI(t *testing.T) {
 		{
 			name:    "internalError",
 			BreedID: breed.BreedID,
+			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+				addAuthorization(t, request, tokenMaker, authorizationTypeBearer, "user", time.Minute)
+			},
 			buildStubs: func(store *mockdb.MockStore) {
 				store.EXPECT().GetBreed(gomock.Any(), gomock.Eq(breed.BreedID)).Times(1).
 					Return(db.Breed{}, sql.ErrConnDone)
@@ -64,6 +108,9 @@ func TestGetBreedAPI(t *testing.T) {
 		{
 			name:    "InvalidID",
 			BreedID: 0,
+			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+				addAuthorization(t, request, tokenMaker, authorizationTypeBearer, "user", time.Minute)
+			},
 			buildStubs: func(store *mockdb.MockStore) {
 				store.EXPECT().GetBreed(gomock.Any(), gomock.Any()).Times(0)
 			},
@@ -89,90 +136,103 @@ func TestGetBreedAPI(t *testing.T) {
 			url := fmt.Sprintf("/breeds/%d", tc.BreedID)
 			req, err := http.NewRequest(http.MethodGet, url, nil)
 			require.NoError(t, err)
+
+			tc.setupAuth(t, req, server.tokenMaker)
 			server.router.ServeHTTP(recorder, req)
 			tc.checkResponse(t, recorder)
 		})
 	}
 }
 
-//func TestCreateBreed(t *testing.T) {
-//	user, _ := randomUser(t)
-//	breed := randomBreed(user.Username)
-//
-//	testCases := []struct {
-//		name          string
-//		body          gin.H
-//		buildStubs    func(store *mockdb.MockStore)
-//		checkResponse func(recorder *httptest.ResponseRecorder)
-//	}{
-//		{
-//			name: "OK",
-//			body: gin.H{
-//				"breed": breed.BreedName,
-//			},
-//			buildStubs: func(store *mockdb.MockStore) {
-//				arg := db.CreateBreedParams{
-//					BreedName: breed.BreedName,
-//					Username:  "",
-//				}
-//				store.EXPECT().CreateBreed(gomock.Any(), gomock.Eq(arg)).Times(1).Return(breed, nil)
-//			},
-//			checkResponse: func(recorder *httptest.ResponseRecorder) {
-//				require.Equal(t, http.StatusOK, recorder.Code)
-//				requireBodyMatchBreed(t, recorder.Body, breed)
-//			},
-//		},
-//		//{
-//		//	name: "NoAuthorization",
-//		//	body: gin.H{
-//		//		"breed": breed.BreedName,
-//		//	},
-//		//	buildStubs: func(store *mockdb.MockStore) {
-//		//		store.EXPECT().CreateBreed(gomock.Any(), gomock.Any()).Times(0)
-//		//	},
-//		//	checkResponse: func(recorder *httptest.ResponseRecorder) {
-//		//		require.Equal(t, http.StatusUnauthorized, recorder.Code)
-//		//	},
-//		//},
-//		{
-//			name: "InternalError",
-//			body: gin.H{
-//				"breed": breed.BreedName,
-//			},
-//			buildStubs: func(store *mockdb.MockStore) {
-//				store.EXPECT().CreateBreed(gomock.Any(), gomock.Any()).Times(1).Return(db.Breed{}, sql.ErrConnDone)
-//			},
-//			checkResponse: func(recorder *httptest.ResponseRecorder) {
-//				require.Equal(t, http.StatusInternalServerError, recorder.Code)
-//			},
-//		},
-//	}
-//	for i := range testCases {
-//		tc := testCases[i]
-//
-//		t.Run(tc.name, func(t *testing.T) {
-//			ctrl := gomock.NewController(t)
-//			defer ctrl.Finish()
-//
-//			store := mockdb.NewMockStore(ctrl)
-//			tc.buildStubs(store)
-//
-//			server := NewServer(store)
-//			recorder := httptest.NewRecorder()
-//
-//			data, err := json.Marshal(tc.body)
-//			require.NoError(t, err)
-//
-//			url := "/breeds"
-//			request, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(data))
-//			require.NoError(t, err)
-//
-//			server.router.ServeHTTP(recorder, request)
-//			tc.checkResponse(recorder)
-//		})
-//	}
-//
-//}
+func TestCreateBreed(t *testing.T) {
+	user, _ := randomUser(t)
+	breed := randomBreed(user.Username)
+
+	testCases := []struct {
+		name          string
+		body          gin.H
+		setupAuth     func(t *testing.T, request *http.Request, tokenMaker token.Maker)
+		buildStubs    func(store *mockdb.MockStore)
+		checkResponse func(recorder *httptest.ResponseRecorder)
+	}{
+		{
+			name: "OK",
+			body: gin.H{
+				"breed": breed.BreedName,
+			},
+			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+				addAuthorization(t, request, tokenMaker, authorizationTypeBearer, user.Username, time.Minute)
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				arg := db.CreateBreedParams{
+					Username:  breed.Username,
+					BreedName: breed.BreedName,
+				}
+				store.EXPECT().CreateBreed(gomock.Any(), gomock.Eq(arg)).Times(1).Return(breed, nil)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+				requireBodyMatchBreed(t, recorder.Body, breed)
+			},
+		},
+		{
+			name: "NoAuthorization",
+			body: gin.H{
+				"breed": breed.BreedName,
+			},
+			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					CreateBreed(gomock.Any(), gomock.Any()).
+					Times(0)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusUnauthorized, recorder.Code)
+			},
+		},
+		{
+			name: "InternalError",
+			body: gin.H{
+				"breed": breed.BreedName,
+			},
+			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+				addAuthorization(t, request, tokenMaker, authorizationTypeBearer, user.Username, time.Minute)
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().CreateBreed(gomock.Any(), gomock.Any()).Times(1).Return(db.Breed{}, sql.ErrConnDone)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusInternalServerError, recorder.Code)
+			},
+		},
+	}
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			store := mockdb.NewMockStore(ctrl)
+			tc.buildStubs(store)
+
+			server := newTestServer(t, store)
+			recorder := httptest.NewRecorder()
+
+			data, err := json.Marshal(tc.body)
+			require.NoError(t, err)
+
+			url := "/breeds"
+			request, err := http.NewRequest("POST", url, bytes.NewReader(data))
+			require.NoError(t, err)
+
+			tc.setupAuth(t, request, server.tokenMaker)
+			server.router.ServeHTTP(recorder, request)
+			tc.checkResponse(recorder)
+		})
+	}
+
+}
 
 // TestListBreedsAPI mocks the API
 func TestListBreedsAPI(t *testing.T) {
@@ -190,6 +250,7 @@ func TestListBreedsAPI(t *testing.T) {
 	testCases := []struct {
 		name          string
 		query         Query
+		setupAuth     func(t *testing.T, request *http.Request, tokenMaker token.Maker)
 		buildStubs    func(store *mockdb.MockStore)
 		checkResponse func(recorder *httptest.ResponseRecorder)
 	}{
@@ -199,10 +260,14 @@ func TestListBreedsAPI(t *testing.T) {
 				pageID:   1,
 				pageSize: n,
 			},
+			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+				addAuthorization(t, request, tokenMaker, authorizationTypeBearer, user.Username, time.Minute)
+			},
 			buildStubs: func(store *mockdb.MockStore) {
 				arg := db.ListBreedsParams{
-					Limit:  int32(n),
-					Offset: 0,
+					Username: user.Username,
+					Limit:    int32(n),
+					Offset:   0,
 				}
 				store.EXPECT().ListBreeds(gomock.Any(), gomock.Eq(arg)).
 					Times(1).Return(breeds, nil)
@@ -213,26 +278,28 @@ func TestListBreedsAPI(t *testing.T) {
 			},
 		},
 
-		//authentication should be implemented first
-
-		//{
-		//	name: "NoAuthorization",
-		//	query: Query{
-		//		pageID:   1,
-		//		pageSize: n,
-		//	},
-		//	buildStubs: func(store *mockdb.MockStore) {
-		//		store.EXPECT().ListBreeds(gomock.Any(), gomock.Any()).Times(0)
-		//	},
-		//	checkResponse: func(recorder *httptest.ResponseRecorder) {
-		//		require.Equal(t, http.StatusUnauthorized, recorder.Code)
-		//	},
-		//},
+		{
+			name: "NoAuthorization",
+			query: Query{
+				pageID:   1,
+				pageSize: n,
+			},
+			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().ListBreeds(gomock.Any(), gomock.Any()).Times(0)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusUnauthorized, recorder.Code)
+			},
+		},
 		{
 			name: "InternalError",
 			query: Query{
 				pageID:   1,
 				pageSize: n,
+			},
+			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+				addAuthorization(t, request, tokenMaker, authorizationTypeBearer, user.Username, time.Minute)
 			},
 			buildStubs: func(store *mockdb.MockStore) {
 				store.EXPECT().ListBreeds(gomock.Any(), gomock.Any()).
@@ -248,6 +315,9 @@ func TestListBreedsAPI(t *testing.T) {
 				pageID:   -1,
 				pageSize: n,
 			},
+			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+				addAuthorization(t, request, tokenMaker, authorizationTypeBearer, user.Username, time.Minute)
+			},
 			buildStubs: func(store *mockdb.MockStore) {
 				store.EXPECT().
 					ListBreeds(gomock.Any(), gomock.Any()).
@@ -262,6 +332,9 @@ func TestListBreedsAPI(t *testing.T) {
 			query: Query{
 				pageID:   1,
 				pageSize: 100000,
+			},
+			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+				addAuthorization(t, request, tokenMaker, authorizationTypeBearer, user.Username, time.Minute)
 			},
 			buildStubs: func(store *mockdb.MockStore) {
 				store.EXPECT().
@@ -293,7 +366,7 @@ func TestListBreedsAPI(t *testing.T) {
 			q.Add("page_id", fmt.Sprintf("%d", tc.query.pageID))
 			q.Add("page_size", fmt.Sprintf("%d", tc.query.pageSize))
 			request.URL.RawQuery = q.Encode()
-
+			tc.setupAuth(t, request, server.tokenMaker)
 			server.router.ServeHTTP(recorder, request)
 			tc.checkResponse(recorder)
 		})
